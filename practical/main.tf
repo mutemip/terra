ee5d24cd0fd5b1b788cc8db45457655947562795
mutemip <paulmutemi2030@gmail.com>
2024-10-17 02:57:42 +0300

    Updated main file:  - Added security group conf.  - Added default vpc conf.  - Added default subnts conf.  - Added SG inbount rule  -  Configured a load balancer.

--- practical/main.tf
+++ practical/main.tf
@@ -20,6 +20,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+ # booststraping aws s3 backend
 resource "aws_s3_bucket" "terraform_state" {
     bucket = "practical-terra-bucket"
     force_destroy = true
@@ -52,13 +53,140 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 }
 
-resource "aws_instance" "webserver" {
+resource "aws_instance" "webserver-1" {
   ami = var.ami
   instance_type = var.instance_type
-  security_groups = ["default"]
+  security_groups = [aws_security_group.instances.name]
   user_data = <<-EOF
     #!/bin/bash
-    echo "Hello, World" > index.html
+    echo "Hello, World 1" > index.html
     python3 -m http.server 8080 &
   EOF
+}
+
+# resource "aws_instance" "webserver-2" {
+#   ami = var.ami
+#   instance_type = var.instance_type
+#   security_groups = [aws_security_group.instances.name]
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     echo "Hello, World 2" > index.html
+#     python3 -m http.server 8080 &
+#   EOF
+# }
+
+# SG should all inbound traffic to the instances
+resource "aws_security_group" "instances" {
+  name = "instance-security-group"
+}
+
+# reference default vpc and subnets
+data "aws_vpc" "default_vpc" {
+    default = true
+}
+
+data "aws_subnets" "default_subnet" {
+    # vpc_id = data.aws_vpc.default_vpc.id
+    filter {
+    name   = "default-for-az"
+    values = ["true"]
+    }
+}
+
+# defne SG rules
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.instances.id
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# setup a load balancer
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port = 80
+  protocol = "HTTP"
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "instances" {
+  name     = "example-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default_vpc.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_target_group_attachment" "webserver-1" {
+  target_group_arn = aws_lb_target_group.instances.arn
+  target_id        = aws_instance.webserver-1.id
+  port             = 8080
+}
+
+# resource "aws_lb_target_group_attachment" "webserver-2" {
+#   target_group_arn = aws_lb_target_group.instances.arn
+#   target_id        = aws_instance.webserver-2.id
+#   port             = 8080
+# }
+
+resource "aws_lb_listener_rule" "instances" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.instances.arn
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "alb-security-group"
+}
+
+resource "aws_security_group_rule" "allow_alb_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_alb_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+resource "aws_lb" "load_balancer" {
+  name               = "web-app-lb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default_subnet.ids
+  security_groups    = [aws_security_group.alb.id]
 }
